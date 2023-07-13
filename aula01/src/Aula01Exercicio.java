c8dc158a398cd6f464b887de9e91541060459689
Thales <thales.souz@outlook.com>
2023-07-12 22:01:18 -0300

    exercicio aula 01

---
+++ aula01/src/Aula01Exercicio.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Aula01Exercicio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em Fahrenheit: ");
+        int temp = sc.nextInt();
+        
+        System.out.printf("A temperatura em Celsius é: %.2f", convertToCelsius(temp));
+
+        sc.close();
+    }
+
+    public static double convertToCelsius(int temp) {
+        return 5 * ((temp - 32) / 9);
+    }
+}
