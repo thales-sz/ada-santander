45cdffe33e3a32f517bc0a6b103dcde8e2926d05
Thales <thales.souz@outlook.com>
2023-07-14 20:32:36 -0300

    aula 02 - exercicio refatorado

--- Módulo 01/aula02/src/Aula02Exercicio.java
+++ Módulo 01/aula02/src/Aula02Exercicio.java
@@ -1,42 +1,34 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Aula02Exercicio {
     public static void main(String[] args) {
+        double finalPrice = 0.00;
+        Scanner sc = new Scanner(System.in);
         String[] fruits = { "peras", "maçãs", "laranjas", "caquis", "tomates" };
-        HashMap<String, Integer> shoppingCart = new HashMap<>();
 
-        populateShoppingCart(fruits, shoppingCart);
+        System.out.println("Bem-vindo ao Mercado do Seu Zé!");
+        
+        for (String fruit : fruits) {
+            System.out.printf("Quantas %s você quer? ", fruit);
+            int quantity = sc.nextInt();
+            finalPrice += calculatePrice(quantity);
+        }
 
-        double finalPrice = calculateFinalPrice(shoppingCart);
+        sc.close();
 
         System.out.printf("O preço final é: R$ %.2f", finalPrice);
     }
 
-    public static double calculateFinalPrice(HashMap<String, Integer> shoppingCart) {
+    public static double calculatePrice(int quantity) {
         double price = 0.00;
 
-        for (int quantity : shoppingCart.values()) {
-            if (quantity > 10) {
-                price += quantity * 1.25;
-            } else {
-                price += quantity * 1.45;
-            }
+        if (quantity > 10) {
+            price += quantity * 1.25;
+        } else {
+            price += quantity * 1.45;
         }
 
         return price;
     }
 
-    public static HashMap<String, Integer> populateShoppingCart(String[] fruits, HashMap<String, Integer> shoppingCart) {
-        Scanner sc = new Scanner(System.in);
-
-        for (String fruit : fruits) {
-            System.out.printf("Quantas %s você quer? ", fruit);
-            int quantity = sc.nextInt();
-            shoppingCart.put(fruit, quantity);
-        }
-
-        sc.close();
-        return shoppingCart;
-    }
 }
