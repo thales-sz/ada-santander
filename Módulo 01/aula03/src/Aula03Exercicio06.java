7ba363fd2785e17eeeac0b42a75fba385a511eff
Thales <thales.souz@outlook.com>
2023-07-17 21:25:25 -0300

    aula 03 - Exercicio 06 - Validação de triangulo valido

--- Módulo 01/aula03/src/Aula03Exercicio06.java
+++ Módulo 01/aula03/src/Aula03Exercicio06.java
@@ -9,14 +9,26 @@ public class Aula03Exercicio06 {
     double b = sc.nextDouble();
     double c = sc.nextDouble();
 
+    if (!isTriangle(a, b, c)) {
+      System.out.println("Não é um triângulo.");
+      sc.close();
+      return;
+    }
+
     System.out.println("Área do triângulo: " + heronsFormula(a, b, c) + " cm²");
 
     sc.close();
   }
 
+  public static boolean isTriangle(double a, double b, double c) {
+    return a < b + c && b < a + c && c < a + b;
+  }
+
   public static double heronsFormula(double a, double b, double c) {
     double p = (a + b + c) / 2;
 
-    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return Math.round(result * 100) / 100.0;
   }
 }
