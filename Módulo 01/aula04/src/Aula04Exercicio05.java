d0b92bdf5916d75254537765ef839c727de8cc91
Thales <thales.souz@outlook.com>
2023-07-19 21:02:06 -0300

    aula 04 - Exercicio 05

---
+++ Módulo 01/aula04/src/Aula04Exercicio05.java
@@ -0,0 +1,54 @@
+public class Aula04Exercicio05 {
+  public static void main(String[] args) {
+    String[] entradas = {
+            "amor",
+            "cocada",
+            "mora",
+            "ovo",
+            "cachorro",
+            "kaka",
+            "saudades",
+            "palalelepipedo",
+            "fofocalizando",
+            "papagaio",
+            "ateiaateiab"
+    };
+
+    int[] saidas = {
+            0,
+            1,
+            0,
+            1,
+            1,
+            -1,
+            2,
+            9,
+            4,
+            4,
+            10
+    };
+
+    for (int i = 0; i < entradas.length; i++) {
+        double resultado = primeiroCaractereUnico(entradas[i]);
+        double esperado = saidas[i];
+
+        System.out.println("Resultado: " + resultado);
+        System.out.println("Esperado: " + esperado);
+        System.out.println(resultado == esperado);
+        System.out.println();
+    }
+  }
+
+  public static int primeiroCaractereUnico(String input) {
+    for(int i = 0; i < input.length(); i++) {
+      char currentChar = input.charAt(i);
+      long count = input.chars().filter(ch -> ch == currentChar).count();
+
+      if (count == 1) {
+        return i;
+      }
+    }
+    
+    return -1;
+  }
+}
