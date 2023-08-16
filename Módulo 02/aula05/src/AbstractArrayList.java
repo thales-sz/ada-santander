989676ae6997de8271a171b137991e64362f917b
Thales <thales.souz@outlook.com>
2023-08-16 20:11:45 -0300

    mod 02 aula 07

--- Módulo 02/aula05/src/AbstractArrayList.java
+++ Módulo 02/aula05/src/AbstractArrayList.java
@@ -9,6 +9,7 @@ public class AbstractArrayList extends AbstractList {
   @Override
   public void add(Object obj) {
     this.array[this.size] = obj;
+    this.size++;
   }
 
   @Override
@@ -22,5 +23,20 @@ public class AbstractArrayList extends AbstractList {
   @Override
   public int size() {
     return this.size;
+  }
+
+  @Override
+  public void remove(int index) {
+    if (index < 0 || index >= this.size) {
+      throw new IndexOutOfBoundsException("Index out of bounds");
+    }
+
+    for (int i = index; i < this.size - 1; i++) {
+      this.array[i] = this.array[i + 1];
+    }
+
+    this.array[this.size - 1] = null;
+
+    this.size--;
   }
 }
--- Módulo 02/aula05/src/AbstractList.java
+++ Módulo 02/aula05/src/AbstractList.java
@@ -6,4 +6,6 @@ public abstract class AbstractList {
   public abstract Object get(int index);
 
   public abstract int size();
+
+  public abstract void remove(int index);
 }
