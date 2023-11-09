d7aece01e959f8ec6f4af1fcc3b88a0a5f882626
Thales <thales.souz@outlook.com>
2023-11-08 22:24:10 -0300

    Desafio 01 - Novos testes

--- Módulo 06/calc/src/test/java/model/CalculadoraTest.java
+++ Módulo 06/calc/src/test/java/model/CalculadoraTest.java
@@ -2,57 +2,115 @@ package model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class CalculadoraTest {
   @Test
   public void deveIniciarDesligada() {
+    // Given
     Calculadora calculadora = new Calculadora();
+    // When
+    // Then
     Assert.assertFalse(calculadora.getStatus());
   }
 
   @Test
   public void deveDesligarCorretamente() {
+    // Given
     Calculadora calculadora = new Calculadora();
+
+    // When
     calculadora.ligar();
     calculadora.desligar();
+
+    // Then
     Assert.assertFalse(calculadora.getStatus());
   }
 
   @Test
   public void deveLigarCorretamente() {
+    // Given
     Calculadora calculadora = new Calculadora();
+
+    // When
     calculadora.ligar();
+
+    // Then
     Assert.assertTrue(calculadora.getStatus());
   }
 
   @Test
   public void naoDeveSerPossivelDividirPorZero() {
+    // Given
     Calculadora calculadora = new Calculadora();
     
+    // When
     calculadora.ligar();
 
     Throwable throwable = Assert.assertThrows(Exception.class, () -> {
       calculadora.divisao(10.0, 0.0);
     });
 
+    // Then
     Assert.assertEquals("Não existe divisão por zero!", throwable.getMessage());
   }
 
   @Test
   public void deveSomarDoisNumeros() {
+    // Given
     Calculadora calculadora = new Calculadora();
     
+    // When
     calculadora.ligar();
 
+    // Then
     Assert.assertEquals((Double) 15.0, calculadora.soma(10.0, 5.0));
   }
 
   @Test
   public void deveSubtrairDoisNumeros() {
+    // Given
     Calculadora calculadora = new Calculadora();
-    
+    // When
     calculadora.ligar();
-
+    // Then
     Assert.assertEquals((Double) 4.9389, calculadora.subtracao(10.72345, 5.7845), 0.0001);
+  }
+
+  @Test
+  public void deveSerUmaCalculadora() {
+    // Given
+    Calculadora calculadora = new Calculadora();
+    // When
+    // Then
+    Assert.assertThat(calculadora, is(instanceOf(Calculadora.class)));
+  }
+
+  @Test
+  public void deveSerNull() {
+    // Given
+    Calculadora calculadora = null;
+    // When
+    // Then
+    Assert.assertNull(calculadora);
+  }
+
+   @Test
+  public void naoDeveSerNull() {
+    // Given
+    Calculadora calculadora = new Calculadora();
+    // When
+    // Then
+    Assert.assertNotNull(calculadora);
+  }
+
+  @Test
+  public void duasCalculadorasNaoDevemSerAsMesmas() {
+    // Given
+    Calculadora calculadora1 = new Calculadora();
+    Calculadora calculadora2 = new Calculadora();
+    // When
+    // Then
+    Assert.assertNotSame(calculadora1, calculadora2);
   }
 }
