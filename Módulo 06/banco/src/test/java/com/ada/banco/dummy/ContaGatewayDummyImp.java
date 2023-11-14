db3433610ef6a967e7a3f5e781d40994368210be
Thales <thales.souz@outlook.com>
2023-11-13 21:56:19 -0300

    new files

--- Módulo 06/banco/src/test/java/com/ada/banco/dummy/ContaGatewayDummyImp.java
+++ Módulo 06/banco/src/test/java/com/ada/banco/dummy/ContaGatewayDummyImp.java
@@ -2,6 +2,18 @@ package com.ada.banco.dummy;
 
 import com.ada.banco.domain.model.Conta;
 
+import java.math.BigDecimal;
+
 public class ContaGatewayDummyImp {
-    public Conta
+    public Conta buscarPorCpf (String cpf) {
+        if (cpf == "123456789") {
+            return new Conta(123456L, 0002L, 5L, BigDecimal.ZERO, "Thales", "123456789");
+        } else {
+            return null;
+        }
+    };
+
+    public Conta salvar(Conta conta) {
+        return new Conta(123456L, 0002L, 5L, BigDecimal.ZERO, "Thales", "123456789");
+    };
 }
